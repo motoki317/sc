f886d562d58d63e273bdedccdd150355378ae2a4
motoki317 <motoki317@gmail.com>
2022-04-08 22:49:34 +0900

    remove samber/lo dependency

--- sc_test.go
+++ sc_test.go
@@ -5,9 +5,33 @@ import (
 	"strconv"
 	"strings"
 	"time"
-
-	"github.com/samber/lo"
 )
+
+// Filter iterates over elements of collection, returning an array of all elements predicate returns truthy for.
+// Copied from github.com/samber/lo.
+func Filter[V any](collection []V, predicate func(V, int) bool) []V {
+	var result []V
+
+	for i, item := range collection {
+		if predicate(item, i) {
+			result = append(result, item)
+		}
+	}
+
+	return result
+}
+
+// Map manipulates a slice and transforms it to a slice of another type.
+// Copied from github.com/samber/lo.
+func Map[T any, R any](collection []T, iteratee func(T, int) R) []R {
+	result := make([]R, len(collection))
+
+	for i, item := range collection {
+		result[i] = iteratee(item, i)
+	}
+
+	return result
+}
 
 type testCase struct {
 	name      string
@@ -20,14 +44,14 @@ var (
 		{name: "LRU cache", cacheOpts: []CacheOption{WithLRUBackend(10)}},
 		{name: "2Q cache", cacheOpts: []CacheOption{With2QBackend(10)}},
 	}
-	strictCaches = lo.Map[testCase, testCase](nonStrictCaches, func(t testCase, _ int) testCase {
+	strictCaches = Map[testCase, testCase](nonStrictCaches, func(t testCase, _ int) testCase {
 		return testCase{
 			name:      "strict " + t.name,
 			cacheOpts: append(append([]CacheOption{}, t.cacheOpts...), EnableStrictCoalescing()),
 		}
 	})
 	allCaches      = append(append([]testCase{}, nonStrictCaches...), strictCaches...)
-	evictingCaches = lo.Filter[testCase](allCaches, func(c testCase, _ int) bool { return !strings.HasSuffix(c.name, "map cache") })
+	evictingCaches = Filter[testCase](allCaches, func(c testCase, _ int) bool { return !strings.HasSuffix(c.name, "map cache") })
 )
 
 func newZipfian(s, v float64, size uint64) func() string {
