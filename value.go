ddd9a5e29ea6c7165e58038f757dc3a03f2c4525
motoki317 <motoki317@gmail.com>
2022-04-17 13:21:53 +0900

    Add value state doc

--- value.go
+++ value.go
@@ -4,6 +4,12 @@ import (
 	"time"
 )
 
+// value represents a cache item.
+//
+// Value can be in one of 3 states:
+// 1. A value is "fresh" for the given freshFor duration.
+// 2. A value is "stale" if it is neither fresh nor expired.
+// 3. A value is "expired" after the given ttl duration.
 type value[V any] struct {
 	v V
 	t time.Time // the time the value was acquired
